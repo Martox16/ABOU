3f7454666cf66e84e88ee2eae7c8170c2d62fb24
Martin <martucyg@gmail.com>
2025-01-06 16:46:00 -0300

    inicio

--- lib/inicio.dart
+++ lib/inicio.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart'; // Importa url_launcher para abrir URLs
-import 'package:auto_size_text/auto_size_text.dart';
-import 'MisDocumentos.dart'; // Importa el archivo MisDocumentos.dart
-import 'MisVehiculos.dart';
-import 'MisTurnos.dart'; // Importa el archivo MisTurnos.dart
-import 'miSalud.dart';
-import 'misCobros.dart';
-import 'misTramites.dart';
-import 'misHijos.dart';
-import 'miTrabajo.dart';
+// Importa las páginas correspondientes
+import 'MisDocumentos.dart'; // MisDocumentos.dart
+import 'MisVehiculos.dart';  // MisVehiculos.dart
+import 'MisTurnos.dart';     // MisTurnos.dart
+import 'miSalud.dart';      // miSalud.dart
+import 'misCobros.dart';    // misCobros.dart
+import 'misTramites.dart';  // misTramites.dart
+import 'misHijos.dart';     // misHijos.dart
+import 'miTrabajo.dart';    // miTrabajo.dart
 
 void main() {
   runApp(MyApp());
@@ -31,33 +31,56 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Lista de las rutas de las imágenes
+    final List<String> imagePaths = [
+      'lib/img/iconoDocumentos.png',
+      'lib/img/iconoVehiculos.png',
+      'lib/img/iconoTrabajo.png',
+      'lib/img/iconoSalud.png',
+      'lib/img/iconoCobros.png',
+      'lib/img/iconoTramites.png',
+      'lib/img/iconoTurnos.png',
+      'lib/img/iconoHijos.png',
+    ];
+
+    final List<Widget> destinationPages = [
+      MisDocumentos(), // Pantalla para los documentos
+      MisVehiculos(),  // Pantalla para vehículos
+      MiTrabajo(),     // Pantalla para trabajo
+      MiSalud(),       // Pantalla para salud
+      MisCobros(),     // Pantalla para cobros
+      MisTramites(),   // Pantalla para trámites
+      MisTurnos(),     // Pantalla para turnos
+      MisHijos(),      // Pantalla para hijos
+    ];
+
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100), // Aumenta la altura del header
+        preferredSize: Size.fromHeight(100),
         child: AppBar(
-          automaticallyImplyLeading: false, // Deshabilita la flecha de navegación
+          automaticallyImplyLeading: false,
           backgroundColor: Color(0xFF372FC0),
           title: Padding(
-            padding: const EdgeInsets.only(top: 16.0), // Ajusta el padding superior del header
+            padding: const EdgeInsets.only(top: 16.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
                   icon: Image.asset(
                     'lib/img/menuNav.png',
-                    width: 32, // Aumenta el tamaño del ícono
+                    width: 32,
                     height: 32,
                   ),
                   onPressed: () {},
                 ),
                 Image.asset(
                   'lib/img/iconoArg.png',
-                  height: 32, // Aumenta el tamaño del logo
+                  height: 32,
                 ),
                 IconButton(
                   icon: Image.asset(
                     'lib/img/logoPerfil.png',
-                    width: 32, // Aumenta el tamaño del ícono
+                    width: 32,
                     height: 32,
                   ),
                   onPressed: () {},
@@ -69,227 +92,68 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: Container(
-        color: Color(0xFFF0F0F0), // Fondo de la página en gris claro
-        
+        color: Color(0xFFF0F0F0),
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                 Image.asset('lib/img/turnosProgramados.jpg'),
-                 
-               Card(
-   color: Color(0xFFF0F0F0),
-  elevation: 0, // Hace que el borde sea invisible
-  child: Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          '¿Qué necesitas hoy?',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 20),
-        GridView.count(
-          crossAxisCount: 4, // Cambiar a 4 columnas
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            ServiceIcon(
-              imagePath: 'lib/img/iconoDocumentos.png',
-              label: '',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) => MisDocumentos(),
-                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                      var begin = Offset(1.0, 0.0);
-                      var end = Offset.zero;
-                      var curve = Curves.ease;
-                      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-                      return SlideTransition(
-                        position: animation.drive(tween),
-                        child: child,
-                      );
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
-                            ServiceIcon(
-                              imagePath: 'lib/img/iconoVehiculos.png',
-                              label: '',
+                Image.asset('lib/img/turnosProgramados.jpg'),
+                Card(
+                  color: Color(0xFFF0F0F0),
+                  elevation: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '¿Qué necesitas hoy?',
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20),
+                        
+                        GridView.builder(
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            crossAxisSpacing: 10.0,
+                            mainAxisSpacing: 10.0,
+                          ),
+                          itemCount: imagePaths.length,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
                               onTap: () {
+                                // Navega a la página correspondiente con una animación personalizada
                                 Navigator.push(
                                   context,
                                   PageRouteBuilder(
-                                    pageBuilder: (context, animation, secondaryAnimation) => MisVehiculos(),
+                                    pageBuilder: (context, animation, secondaryAnimation) {
+                                      return destinationPages[index];
+                                    },
                                     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                      var begin = Offset(1.0, 0.0);
-                                      var end = Offset.zero;
-                                      var curve = Curves.ease;
+                                      // Definimos la animación de transición
+                                      const begin = Offset(1.0, 0.0); // La nueva pantalla viene desde la derecha
+                                      const end = Offset.zero; // La nueva pantalla se establece en su posición final
+                                      const curve = Curves.easeInOut; // Curva de la animación
 
                                       var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                                      var offsetAnimation = animation.drive(tween);
 
-                                      return SlideTransition(
-                                        position: animation.drive(tween),
-                                        child: child,
-                                      );
+                                      // Aplicamos la animación al contenido de la pantalla
+                                      return SlideTransition(position: offsetAnimation, child: child);
                                     },
                                   ),
                                 );
                               },
-                            ),
-                            ServiceIcon(
-                              imagePath: 'lib/img/iconoSalud.png',
-                              label: '',
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  PageRouteBuilder(
-                                    pageBuilder: (context, animation, secondaryAnimation) => MiSalud(),
-                                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                      var begin = Offset(1.0, 0.0);
-                                      var end = Offset.zero;
-                                      var curve = Curves.ease;
-
-                                      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-                                      return SlideTransition(
-                                        position: animation.drive(tween),
-                                        child: child,
-                                      );
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
-                            ServiceIcon(
-                              imagePath: 'lib/img/iconoTrabajo.png',
-                              label: '',
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  PageRouteBuilder(
-                                    pageBuilder: (context, animation, secondaryAnimation) => MiTrabajo(),
-                                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                      var begin = Offset(1.0, 0.0);
-                                      var end = Offset.zero;
-                                      var curve = Curves.ease;
-
-                                      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-                                      return SlideTransition(
-                                        position: animation.drive(tween),
-                                        child: child,
-                                      );
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
-                            ServiceIcon(
-                              imagePath: 'lib/img/iconoTurnos.png',
-                              label: '',
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  PageRouteBuilder(
-                                    pageBuilder: (context, animation, secondaryAnimation) => MisTurnos(),
-                                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                      var begin = Offset(1.0, 0.0);
-                                      var end = Offset.zero;
-                                      var curve = Curves.ease;
-
-                                      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-                                      return SlideTransition(
-                                        position: animation.drive(tween),
-                                        child: child,
-                                      );
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
-                            ServiceIcon(
-                              imagePath: 'lib/img/iconoCobros.png',
-                              label: '',
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  PageRouteBuilder(
-                                    pageBuilder: (context, animation, secondaryAnimation) => MisCobros(),
-                                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                      var begin = Offset(1.0, 0.0);
-                                      var end = Offset.zero;
-                                      var curve = Curves.ease;
-
-                                      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-                                      return SlideTransition(
-                                        position: animation.drive(tween),
-                                        child: child,
-                                      );
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
-                            ServiceIcon(
-                              imagePath: 'lib/img/iconoTramites.png',
-                              label: '',
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  PageRouteBuilder(
-                                    pageBuilder: (context, animation, secondaryAnimation) => MisTramites(),
-                                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                      var begin = Offset(1.0, 0.0);
-                                      var end = Offset.zero;
-                                      var curve = Curves.ease;
-
-                                      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-                                      return SlideTransition(
-                                        position: animation.drive(tween),
-                                        child: child,
-                                      );
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
-                            ServiceIcon(
-                              imagePath: 'lib/img/iconoHijos.png',
-                              label: '',
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  PageRouteBuilder(
-                                    pageBuilder: (context, animation, secondaryAnimation) => MisHijos(),
-                                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                      var begin = Offset(1.0, 0.0);
-                                      var end = Offset.zero;
-                                      var curve = Curves.ease;
-
-                                      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-                                      return SlideTransition(
-                                        position: animation.drive(tween),
-                                        child: child,
-                                      );
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
-                          ],
+                              child: Image.asset(
+                                imagePaths[index],
+                                fit: BoxFit.cover,
+                              ),
+                            );
+                          },
                         ),
                       ],
                     ),
@@ -306,20 +170,20 @@ class HomePage extends StatelessWidget {
           double dx = details.globalPosition.dx;
 
           if (dx <= footerWidth * 0.25) {
-            Navigator.pushNamed(context, '/inicio'); // Navegar a inicio
+            Navigator.pushNamed(context, '/inicio');
           } else if (dx <= footerWidth * 0.50) {
-            Navigator.pushNamed(context, '/novedades'); // Navegar a novedades
+            Navigator.pushNamed(context, '/novedades');
           } else {
             String url = 'https://wa.me/541139101010?text=¡Hola!%20Quiero%20hacerte%20una%20consulta.';
-            launchURL(url); // Abrir el URL de WhatsApp
+            launchURL(url);
           }
         },
         child: Container(
-          height: 60, // Ajusta la altura del footer según sea necesario
+          height: 60,
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage('lib/img/footer.png'),
-              fit: BoxFit.cover, // Ajusta la imagen para que cubra el footer
+              fit: BoxFit.cover,
             ),
           ),
         ),
@@ -333,50 +197,5 @@ class HomePage extends StatelessWidget {
     } else {
       throw 'No se pudo abrir $url';
     }
-  }
-}
-
-class ServiceIcon extends StatelessWidget {
-  final String imagePath;
-  final String label;
-  final VoidCallback? onTap;
-
-  const ServiceIcon({
-    required this.imagePath,
-    required this.label,
-    this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double imageSize = constraints.maxWidth * 0.6; // Ajusta el tamaño basado en el espacio disponible
-        return InkWell(
-          onTap: onTap, // Asigna el callback onTap
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                imagePath,
-                width: imageSize,
-                height: imageSize,
-              ),
-              SizedBox(height: 10),
-              Flexible(
-                child: Text(
-                  label,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14, // Ajusta el tamaño de la fuente
-                    overflow: TextOverflow.ellipsis, // Asegura que el texto no se divida
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
   }
 }
